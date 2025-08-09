72719a9615be94011cb02427d0efb944f9ffcefe
hisham4o4 <hisham20muhammed@gmail.com>
2025-08-09 15:32:13 +1000

    db update

--- server/database.js
+++ server/database.js
@@ -2,25 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL, 
+    family: 4, 
 });
 
-const initializeDb = async () => {
-    console.log('Connecting to PostgreSQL...');
-    const client = await pool.connect();
-    console.log('Connected to PostgreSQL database.');
-
-    // Note: Unlike SQLite, we don't run CREATE TABLE on every connect.
-    // You should run these queries once using the Supabase SQL Editor.
-    // Go to your Supabase project -> SQL Editor -> "New query" and paste the
-    // contents from the "CREATE TABLE..." section in the original database.js file.
-
-    client.release();
-};
 
 // Run the CREATE TABLE scripts in the Supabase SQL Editor one time!
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
-    initializeDb,
 };
