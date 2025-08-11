7e5a457ebaf7ad50dcf7dc38db76401e5838fcba
hisham4o4 <hisham20muhammed@gmail.com>
2025-08-11 15:43:15 +1000

    updates1.0bugfix3

--- client/src/AdminDashboard.js
+++ client/src/AdminDashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AdminFlowChart from './AdminFlowChart';
 import DashboardGreeting from './DashboardGreeting'; // Assuming this component exists
-
+import WorkflowTaskView from './WorkflowTaskView';
 const API_URL = process.env.REACT_APP_API_URL;
 
 const AdminDashboard = ({ token }) => {
